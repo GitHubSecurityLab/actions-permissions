e16c896e9879868193d9b9375e89216c53c87d79
Jaroslav Lobačevski <jarlob@github.com>
2024-07-04 18:12:39 +0200

    Update setup.sh

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -90,7 +90,7 @@ elif [ "$RUNNER_OS" = "Linux" ]; then
   sudo -u mitmproxyuser -H bash -e -c 'cd ~ && python3.9 get-pip.py && ~/.local/bin/pip3.9 install --user mitmproxy'
 
   sudo cp mitm_plugin.py /home/mitmproxyuser/mitm_plugin.py
-  sudo -u mitmproxyuser -H bash -e -c "cd /home/mitmproxyuser && /home/mitmproxyuser/.local/bin/mitmdump --mode transparent --showhost --set block_global=false -q -s /home/mitmproxyuser/mitm_plugin.py --set output='/home/mitmproxyuser/out.txt' --set token='$INPUT_TOKEN' --set hosts='api.github.com,github.com' --set debug='$RUNNER_DEBUG' --set ACTIONS_ID_TOKEN_REQUEST_URL='$ACTIONS_ID_TOKEN_REQUEST_URL' --set ACTIONS_ID_TOKEN_REQUEST_TOKEN='$ACTIONS_ID_TOKEN_REQUEST_TOKEN' --set GITHUB_REPOSITORY_ID='$GITHUB_REPOSITORY_ID' --set GITHUB_REPOSITORY='$GITHUB_REPOSITORY' &"
+  sudo -u mitmproxyuser -H bash -e -c "cd /home/mitmproxyuser && /home/mitmproxyuser/.local/bin/mitmdump --mode transparent --allow-hosts 'github.com' --showhost --set block_global=false -q -s /home/mitmproxyuser/mitm_plugin.py --set output='/home/mitmproxyuser/out.txt' --set token='$INPUT_TOKEN' --set hosts='api.github.com,github.com' --set debug='$RUNNER_DEBUG' --set ACTIONS_ID_TOKEN_REQUEST_URL='$ACTIONS_ID_TOKEN_REQUEST_URL' --set ACTIONS_ID_TOKEN_REQUEST_TOKEN='$ACTIONS_ID_TOKEN_REQUEST_TOKEN' --set GITHUB_REPOSITORY_ID='$GITHUB_REPOSITORY_ID' --set GITHUB_REPOSITORY='$GITHUB_REPOSITORY' &"
 
   # wait for mitmdump to start and generate CA certificate
   counter=0
@@ -117,7 +117,7 @@ elif [ "$RUNNER_OS" = "Linux" ]; then
   # set environment variable for the Elixir Hex package manager to use the certificate
   echo "HEX_CACERTS_PATH=/home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem" >> $GITHUB_ENV
   # set environment variable for Maven to use the certificate
-  echo "MAVEN_OPTS=-Djavax.net.ssl.trustStore=/home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem -Djavax.net.ssl.trustStoreType=pkcs12 -Djavax.net.debug=ssl,keymanager" >> $GITHUB_ENV
+  # echo "MAVEN_OPTS=-Djavax.net.ssl.trustStore=/home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem -Djavax.net.ssl.trustStoreType=pkcs12 -Djavax.net.debug=ssl,keymanager" >> $GITHUB_ENV
 
   # setup global redirection
   sudo sysctl -w net.ipv4.ip_forward=1
