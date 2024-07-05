ab5a936664e2c13f332ab63294f31c6ff6a84056
Jaroslav Lobačevski <jarlob@github.com>
2024-07-05 09:23:50 +0200

    Update setup.sh

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -100,7 +100,7 @@ elif [ "$RUNNER_OS" = "Linux" ]; then
                                                                               `#--allow-hosts 'github.com'` \
                                                                               --set block_global=false \
                                                                               `#-q` \
-                                                                              --termlog_verbosity=debug \
+                                                                              --termlog-verbosity=debug \
                                                                               -s /home/mitmproxyuser/mitm_plugin.py \
                                                                               --set output='/home/mitmproxyuser/out.txt' \
                                                                               --set token='$INPUT_TOKEN' \
